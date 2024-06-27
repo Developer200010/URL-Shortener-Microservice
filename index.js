09ae592c50ace6da671e25f20101bcaab9f34ea8
chandan vishwakarma <chandanvishw11002@gmail.com>
2024-06-27 19:31:42 +0530

    done

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
@@ -14,10 +17,53 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const originalUrls=[];
+const shortUrls = [];
+
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  console.log(originalUrls);
+  console.log(shortUrls);
+  const url = req.body.url;
+  const foundIndex = originalUrls.indexOf(url);
+  
+  if(!url.includes("https://") && !url.includes("http://")){
+    return res.json({
+      error: "invalid url"
+    })
+  }
+
+  if(foundIndex < 0){
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length);
+    return res.json({
+      original_url:url,
+      short_url : shortUrls.length - 1,
+    });  
+  }
+  return res.json({
+    original_url:url,
+    short_url:shortUrls[foundIndex]
+  })
+  
 });
+
+app.get("/api/shorturl/:shorturl", function(req,res){
+ const shorturl = parseInt(req.params.shorturl);
+ console.log(shorturl);
+ const foundIndex = shortUrls.indexOf(shorturl);
+ console.log(foundIndex);
+ 
+ if(foundIndex < 0){
+  return res.json({
+    error : "No short URL found for the given input",
+  })
+ }
+
+ res.redirect(originalUrls[foundIndex]);
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
